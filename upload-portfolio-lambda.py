8682412203d780010a15324921acd42e50b04585
davedewit <dave@dewit.com.au>
2019-05-26 23:02:26 +1000

    update

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,13 +1,9 @@
 import boto3
 
-import StringIO
-import mimetypes
-
 def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:ap-southeast-2:114366766218:DeployPortfolioTopic')
 
-    s3 = boto3.resource('s3')
 
-    portfolio_bucket = s3.Bucket('portfolio.dewit.com.au')
-    portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    print "Job Done!"
+    topic.publish(Subject="Test #2", Message="Portfolio deployed successfully!.")

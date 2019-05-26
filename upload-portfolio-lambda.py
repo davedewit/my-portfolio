dacb5f240fd8174a46c95534851af858f46342a6
davedewit <dave@dewit.com.au>
2019-05-26 16:24:55 +1000

    update

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,6 +1,6 @@
 import boto3
-import StringIO
 import zipfile
+import StringIO
 import mimetypes
 
 def lambda_handler(event, context):
@@ -16,18 +16,17 @@ def lambda_handler(event, context):
 
         if job:
             for artifact in job["data"]["inputArtifacts"]:
-                if artifact["name"] == "MyAppBuild":
+                if artifact["name"] == "buildportfolio":
                     location = artifact["location"]["s3Location"]
 
         print "building portfolio from " + str(location)
-
         s3 = boto3.resource('s3')
 
         portfolio_bucket = s3.Bucket('portfolio.dewit.com.au')
         build_bucket = s3.Bucket(location["bucketName"])
 
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj(location['objectKey'], portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
